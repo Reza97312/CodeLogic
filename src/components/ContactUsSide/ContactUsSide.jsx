63fad8626584f495701211d91eddbb972c2dc485
Reza97312 <rezakazemi1381@yahoo.com>
2026-05-17 02:45:59 +0330

    Manual upload

--- src/components/ContactUsSide/ContactUsSide.jsx
+++ src/components/ContactUsSide/ContactUsSide.jsx
@@ -5,14 +5,14 @@ import { useTranslation } from "react-i18next";
 const ContactUsSide = () => {
   const { t } = useTranslation();
   return (
-    <div className="w-full flex flex-col gap-3 md:pe-10">
-      <h2 className="text-[32px] font-bold dark:text-[#ffff] ">
+    <div className="  w-full flex flex-col gap-3 md:pe-10">
+      <h2 className=" text-md sm:text-2xl md:text-xl lg:text-2xl  mb-5 sm:mb-0 md:mb-2 lg:mb-0 font-bold dark:text-[#ffff] ">
         {t("contactUs.p1")}
-        <span className="text-[#008C78] dark:text-[#008C78]">
+        <span className="text-[#008C78] px-2 dark:text-[#008C78]">
           {t("contactUs.p2")}
         </span>
       </h2>
-      <p className="text-[#1E1E1E] dark:text-[#dddd] text-[16px] pe-8 ">
+      <p className="text-[#1E1E1E] dark:text-[#dddd] text-sm sm:text-[16px] pe-0 sm:pe-8 ">
         {t("contactUs.p3")}
       </p>
       <div className="flex flex-col gap-3 mt-5">
@@ -22,7 +22,7 @@ const ContactUsSide = () => {
             alt=""
             className=" bg-[#008C78] p-2 rounded-full  "
           />
-          <h3 className="font-bold text-[18px] dark:text-[#dddd] ">
+          <h3 className="font-bold text-md sm:text-[18px] dark:text-[#dddd] ">
             011-123-45-678
           </h3>
         </div>
@@ -32,7 +32,7 @@ const ContactUsSide = () => {
             alt=""
             className=" bg-[#008C78] p-2 rounded-full  "
           />
-          <h3 className="font-bold text-[18px] dark:text-[#dddd] ">
+          <h3 className="font-bold text-md sm:text-[18px] dark:text-[#dddd] ">
             CodeLogic@gmail.com
           </h3>
         </div>
